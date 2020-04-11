706e8f5dbeeb33e9ea6781f4bbfc47f13f53df6b
ben <benjamindrolls@gmail.com>
2020-04-11 12:34:16 -0400

    added api requst and key

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+      getLanguageAPI() {
+        fetch(http://api.languagelayer.com/detect?access_key=b9478dbdb301892dc09ea0c6fbd6c870)
+        .then(res => res.json())
+        .then((data)=> {
+          this.setState({language: data})
+        })
+        .catch(console.log)
+      }
+
       <header className="App-header">
         <p>
           Header
